7bdc3b5730ab7156b6db6f06e653e440a89f6f61
lalitakapadia <lalita.kapadia@gmail.com>
2025-02-11 17:53:52 +0000

    metal inspection

--- src/pages/MetalInspection.jsx
+++ src/pages/MetalInspection.jsx
@@ -7,117 +7,114 @@ const MetalInspection = () => {
 
   return (
 
-    
-    
-    <div className="overflow-hidden py-8">
-        <div className="px-6 lg:px-8">
-            <div className="mx-auto max-w-2xl lg:max-w-7xl">
-                <div>
-                    <h3
-                        className="mt-2 text-pretty text-4xl font-medium tracking-tighter text-gray-950 data-[dark]:text-white sm:text-6xl">
-                        Metal Inspection Trusted by Clients.</h3>
-                </div>
-            </div>
+    <>
+    <div class="mt-16 px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:max-w-7xl">
+            <h1
+                class="text-pretty text-3xl font-medium tracking-tighter text-gray-950 data-[dark]:text-white sm:text-6xl">
+                Metal Inspection</h1>
+            <p class="mt-6 max-w-3xl text-2xl font-medium text-gray-500">
+                We are DGFT approved Pre-shipment inspection agency authorized to conduct a metal scrap inspection.</p>
         </div>
-        
-        <div
-            className="mt-16 flex gap-8 px-[var(--scroll-padding)] [scrollbar-width:none] [&amp;::-webkit-scrollbar]:hidden snap-x snap-mandatory overflow-x-auto overscroll-x-contain scroll-smooth [--scroll-padding:max(theme(spacing.6),calc((100vw-theme(maxWidth.2xl))/2))] lg:[--scroll-padding:max(theme(spacing.8),calc((100vw-theme(maxWidth.7xl))/2))]">
-            <div id="card-1"
-                className="relative flex aspect-[9/16] w-72 shrink-0 snap-start scroll-ml-[var(--scroll-padding)] flex-col justify-end overflow-hidden rounded-3xl sm:aspect-[12/4] sm:w-272">
-                <img alt="" src="./public/images/metalinspection.jpg"
-                    className="absolute inset-x-0 top-0 aspect-square w-full object-cover"></img>
-                <div aria-hidden="true"
-                    className="absolute inset-0 rounded-3xl bg-gradient-to-t from-black from-[calc(7/16*100%)] ring-1 ring-inset ring-gray-950/10 sm:from-25%">
-                </div>
-                <figure className="relative p-10">
-                    <blockquote>
-                        <p className="relative text-xl/7 text-white"><span aria-hidden="true"
-                                className="absolute -translate-x-full">“</span>Metal scrap inspection and testing for composition, grade, and purity, is an important process undertaken before any cargo is imported into the country. The DGFT approved Pre-shipment inspection agency must certify that the cargo being imported is free of any unwanted material such as explosives or radioactive elements. But, metal scrap inspection in itself is more than just a visual inspection on site. During metal scrap inspection, many aspects regarding quality checks need to be considered when dealing with ferrous or non-ferrous metals. Scrap metals are complicated materials to inspect out of recyclables since we need to take into account the impurities, corrosion, and fatigue. Not to mention there needs to be a thorough investigation on toxicities, such as oil and chemical residue that can harm the scrap metal.<span aria-hidden="true"
-                                className="absolute">”</span></p>
-                    </blockquote>
-                </figure>
-            </div>
-            <div id="card-2"
-                className="relative flex opacity-50 aspect-[9/16] w-72 shrink-0 snap-start scroll-ml-[var(--scroll-padding)] flex-col justify-end overflow-hidden rounded-3xl sm:aspect-[10/4] sm:w-248">
-                <img alt="" src="/testimonials/conor-neville.jpg"
-                    className="absolute inset-x-0 top-0 aspect-square w-full object-cover"></img>
-                <div aria-hidden="true"
-                    className="absolute inset-0 rounded-3xl bg-gradient-to-t from-black from-[calc(7/16*100%)] ring-1 ring-inset ring-gray-950/10 sm:from-25%">
-                </div>
-                <figure className="relative p-10">
-                    <blockquote>
-                        <p className="relative text-xl/7 text-white"><span aria-hidden="true"
-                                className="absolute -translate-x-full">“</span>Radiant made undercutting all of our
-                            competitors an absolute breeze.<span aria-hidden="true" className="absolute">”</span></p>
-                    </blockquote>
-                    <figcaption className="mt-6 border-t border-white/20 pt-6">
-                        <p className="text-sm/6 font-medium text-white">Conor Neville</p>
-                        <p className="text-sm/6 font-medium"><span
-                                className="bg-gradient-to-r from-[#fff1be] from-[28%] via-[#ee87cb] via-[70%] to-[#b060ff] bg-clip-text text-transparent">Head
-                                of Customer Success, TaxPal</span></p>
-                    </figcaption>
-                </figure>
-            </div>
-            <div id="card-3"
-                className="relative flex aspect-[9/16] w-72 shrink-0 snap-start scroll-ml-[var(--scroll-padding)] flex-col justify-end overflow-hidden rounded-3xl sm:aspect-[10/4] sm:w-248">
-                <img alt="" src="/testimonials/amy-chase.jpg"
-                    className="absolute inset-x-0 top-0 aspect-square w-full object-cover"></img>
-                <div aria-hidden="true"
-                    className="absolute inset-0 rounded-3xl bg-gradient-to-t from-black from-[calc(7/16*100%)] ring-1 ring-inset ring-gray-950/10 sm:from-25%">
-                </div>
-                <figure className="relative p-10">
-                    <blockquote>
-                        <p className="relative text-xl/7 text-white"><span aria-hidden="true"
-                                className="absolute -translate-x-full">“</span>We closed a deal in literally a few
-                            minutes because we knew their exact budget.<span aria-hidden="true"
-                                className="absolute">”</span></p>
-                    </blockquote>
-                    <figcaption className="mt-6 border-t border-white/20 pt-6">
-                        <p className="text-sm/6 font-medium text-white">Amy Chase</p>
-                        <p className="text-sm/6 font-medium"><span
-                                className="bg-gradient-to-r from-[#fff1be] from-[28%] via-[#ee87cb] via-[70%] to-[#b060ff] bg-clip-text text-transparent">Head
-                                of GTM, Pocket</span></p>
-                    </figcaption>
-                </figure>
-            </div>
-            
-            <div className="w-[42rem] shrink-0 sm:w-[54rem]"></div>
-        </div>
-
-        <div className="mt-16 px-6 lg:px-8">
-            <div className="mx-auto max-w-2xl lg:max-w-7xl">
-                <div className="flex justify-between">
-                    <div>
-                        <p className="max-w-sm text-sm/6 text-gray-600">Join the best sellers in the business and start
-                            using Radiant to hit your targets today.</p>
-                        <div className="mt-2"><a
-                                className="inline-flex items-center gap-2 text-sm/6 font-medium text-pink-600"
-                                data-headlessui-state="" href="#">Get started<svg xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon"
-                                    className="size-5">
-                                    <path fillRule="evenodd"
-                                        d="M2 10a.75.75 0 0 1 .75-.75h12.59l-2.1-1.95a.75.75 0 1 1 1.02-1.1l3.5 3.25a.75.75 0 0 1 0 1.1l-3.5 3.25a.75.75 0 1 1-1.02-1.1l2.1-1.95H2.75A.75.75 0 0 1 2 10Z"
-                                        clipRule="evenodd"></path>
-                                </svg></a></div>
+    </div>
+    
+    <div class="mx-2 my-24 rounded-4xl bg-gray-900 bg-[url(/dot-texture.svg)] pb-24 pt-72 lg:pt-36">
+        <div class="px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:max-w-7xl">
+                <div class="grid grid-cols-1 lg:grid-cols-[384px_1fr_1fr]">
+                    <div class="-mt-96 lg:-mt-52">
+                        <div
+                            class="-m-2 rounded-4xl bg-white/15 shadow-[inset_0_0_2px_1px_#ffffff4d] ring-1 ring-black/5 max-lg:mx-auto max-lg:max-w-xs">
+                            <div class="rounded-4xl p-2 shadow-md shadow-black/5">
+                                <div
+                                    class="overflow-hidden rounded-3xl shadow-2xl outline outline-1 -outline-offset-1 outline-black/10">
+                                    <img alt="" src="./public/images/inspection2.jpg"
+                                        class="aspect-[3/4] w-full object-cover">
+                                            </img>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="hidden sm:flex sm:gap-2">
-                        <button data-active="true"
-                            aria-label="Scroll to testimonial from Tina Yards"
-                            className="size-2.5 rounded-full border border-transparent bg-gray-300 transition data-[active]:bg-gray-400 data-[hover]:bg-gray-400 forced-colors:data-[active]:bg-[Highlight] forced-colors:data-[focus]:outline-offset-4"
-                            type="button" data-headlessui-state=""></button>
-                        <button
-                            aria-label="Scroll to testimonial from Conor Neville"
-                            className="size-2.5 rounded-full border border-transparent bg-gray-300 transition data-[active]:bg-gray-400 data-[hover]:bg-gray-400 forced-colors:data-[active]:bg-[Highlight] forced-colors:data-[focus]:outline-offset-4"
-                            type="button" data-headlessui-state=""></button>
-                        <button
-                            aria-label="Scroll to testimonial from Amy Chase"
-                            className="size-2.5 rounded-full border border-transparent bg-gray-300 transition data-[active]:bg-gray-400 data-[hover]:bg-gray-400 forced-colors:data-[active]:bg-[Highlight] forced-colors:data-[focus]:outline-offset-4"
-                            type="button" data-headlessui-state=""></button>
+                    <div class="flex lg:-mt-20 max-lg:mt-16 lg:col-span-2 lg:px-16">
+                        <figure class="mx-auto flex max-w-xl flex-col gap-16 max-lg:text-center">
+                            <blockquote>
+                                <p
+                                    class="relative text-3xl tracking-tight text-white before:absolute before:-translate-x-full before:content-['“'] after:absolute after:content-['”'] lg:text-xl">
+                                    Metal scrap inspection and testing for composition, grade, and purity, is an important process undertaken before any cargo is imported into the country. The DGFT approved Pre-shipment inspection agency must certify that the cargo being imported is free of any unwanted material such as explosives or radioactive elements. But, metal scrap inspection in itself is more than just a visual inspection on site. During metal scrap inspection, many aspects regarding quality checks need to be considered when dealing with ferrous or non-ferrous metals. Scrap metals are complicated materials to inspect out of recyclables since we need to take into account the impurities, corrosion, and fatigue. Not to mention there needs to be a thorough investigation on toxicities, such as oil and chemical residue that can harm the scrap metal.
+                                </p>
+                            </blockquote>
+                        </figure>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="mx-2 my-24 rounded-4xl bg-gray-900 bg-[url(/dot-texture.svg)] pb-24 pt-72 lg:pt-36">
+        <div class="px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:max-w-7xl">
+                <div class="grid grid-cols-1 lg:grid-cols-[384px_1fr_1fr]">
+                    <div class="-mt-96 lg:-mt-52">
+                        <div
+                            class="-m-2 rounded-4xl bg-white/15 shadow-[inset_0_0_2px_1px_#ffffff4d] ring-1 ring-black/5 max-lg:mx-auto max-lg:max-w-xs">
+                            <div class="rounded-4xl p-2 shadow-md shadow-black/5">
+                                <div
+                                    class="overflow-hidden rounded-3xl shadow-2xl outline outline-1 -outline-offset-1 outline-black/10">
+                                    <img alt="" src="./public/images/inspection2.jpg"
+                                        class="aspect-[3/4] w-full object-cover">
+                                            </img>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex lg:-mt-20 max-lg:mt-16 lg:col-span-2 lg:px-16">
+                        <figure class="mx-auto flex max-w-xl flex-col gap-16 max-lg:text-center">
+                            <blockquote>
+                                <p
+                                    class="relative text-3xl tracking-tight text-white before:absolute before:-translate-x-full before:content-['“'] after:absolute after:content-['”'] lg:text-xl">
+                                    MELT Enterprise is a DGFT approved Pre-shipment inspection agency authorized to conduct a metal scrap inspection. We inspect the cargo at the time of loading and ensure that the containers destined for India and free from any hazards. We have our own testing facilities and are authorized to provide Pre-shipment Inspection Certification in 60+ countries. This means we are authorized to check for the presence of arms and ammunition, mines, shells, cartridges, or any other explosive in any form along with radiation (Gamma & Neutron) level and subsequently should carry a Pre-Shipment inspection certificate upon arriving in India.
+                                </p>
+                            </blockquote>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+{/*     
+    <div class="relative h-80 shrink-0">
+        <div aria-hidden="true" class="relative h-full overflow-hidden">
+            <div class="absolute inset-0">
+                <div style="--opacity:3%;width:528px;height:528px"
+                    class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[radial-gradient(circle,transparent_25%,color-mix(in_srgb,_theme(colors.blue.500)_var(--opacity),transparent)_100%)] ring-1 ring-inset ring-blue-500/[8%]">
+                </div>
+                <div style="--opacity:5%;width:400px;height:400px"
+                    class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[radial-gradient(circle,transparent_25%,color-mix(in_srgb,_theme(colors.blue.500)_var(--opacity),transparent)_100%)] ring-1 ring-inset ring-blue-500/[8%]">
+                </div>
+                <div style="--opacity:5%;width:272px;height:272px"
+                    class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[radial-gradient(circle,transparent_25%,color-mix(in_srgb,_theme(colors.blue.500)_var(--opacity),transparent)_100%)] ring-1 ring-inset ring-blue-500/[8%]">
+                </div>
+                <div style="--opacity:10%;width:144px;height:144px"
+                    class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[radial-gradient(circle,transparent_25%,color-mix(in_srgb,_theme(colors.blue.500)_var(--opacity),transparent)_100%)] ring-1 ring-inset ring-blue-500/[8%]">
+                </div>
+                <div class="absolute inset-0 bg-gradient-to-t from-white to-35%"></div>
+            </div>
+            <div class="absolute left-1/2 h-full w-[26rem] -translate-x-1/2">
+                <div
+                    class="absolute left-44 top-32 flex size-16 items-center justify-center rounded-full bg-white shadow ring-1 ring-black/5">
+                    </div><img alt="" src="./public/images/linkedin-color-svgrepo-com.svg"
+                    style="left:360px;top:144px;transform:none"
+                    class="absolute size-16 rounded-full bg-white shadow ring-1 ring-black/5"></img>
+                    <img
+                    alt="" src="./public/images/linkedin-color-svgrepo-com.svg"
+                    style="left:285px;top:20px;transform:none"
+                    class="absolute size-16 rounded-full bg-white shadow ring-1 ring-black/5"></img>
+                    
+            </div>
+        </div>
+    </div> */}
+    
+    </>
     
   );
 };
