99805b1880266168e693cf027aea5d0243a6060e
lalitakapadia <lalita.kapadia@gmail.com>
2025-02-27 16:56:48 +0000

    react refactor for deployment to try routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Outlet } from 'react-router-dom'
+
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import ContactUs from './pages/ContactUs';
@@ -14,21 +14,25 @@ import Footer from './components/Footer';
 import Clients from './pages/Clients';
 function App() {
   return (
-    <Router>
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-          <Route path= "/about-us" element={<AboutUs/>} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/responsibilities" element={<Responsibilities />} />
-          <Route path="/metal-inspection" element={<MetalInspection />} />
-          <Route path="/metal-import" element={<MetalImport />} />
-          <Route path="/clients" element={<Clients />} />
-        </Routes>
-      </main> 
-      <Footer/> 
-    </Router>
+        <>
+          <Outlet />
+          <Footer/>
+        </>
+    // <Router>
+    //   <main>
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/contact-us" element={<ContactUs />} />
+    //       <Route path= "/about-us" element={<AboutUs/>} />
+    //       <Route path="/services" element={<Services />} />
+    //       <Route path="/responsibilities" element={<Responsibilities />} />
+    //       <Route path="/metal-inspection" element={<MetalInspection />} />
+    //       <Route path="/metal-import" element={<MetalImport />} />
+    //       <Route path="/clients" element={<Clients />} />
+    //     </Routes>
+    //   </main> 
+    //   <Footer/> 
+    // </Router>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,56 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ContactUs from './pages/ContactUs';
+import AboutUs from './components/AboutUs';
+import Services from './pages/Services';
+import Responsibilities from './pages/Responsibilities';
+import MetalInspection from './pages/MetalInspection';
+import MetalImport from './pages/MetalImport';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import Clients from './pages/Clients';
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/about-us',
+        element: <AboutUs/>,
+      },
+      {
+        path: '/services',
+        element: <Services/>,
+      },
+      {
+        path: '/responsibilities',
+        element: <Responsibilities/>,
+      },
+      {
+        path: '/metal-inspection',
+        element: <MetalInspection/>,
+      },
+      {
+        path: '/metal-import',
+        element: <MetalImport/>,
+      },
+      {
+        path: '/contact-us',
+        element: <ContactUs/>,
+      },
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 )
