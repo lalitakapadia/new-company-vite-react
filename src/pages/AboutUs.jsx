447282294ebde5193e90eff7bde794809e73049a
lalitakapadia <lalita.kapadia@gmail.com>
2025-03-04 10:33:36 +0000

    added overflow hidden so that mobile site won't scroll to the right side

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -39,7 +39,7 @@ export default function AboutUs() {
       </Helmet>
 
       <div className="text-gray-800">
-      <div class="relative mx-auto ">
+      <div class="relative mx-auto overflow-hidden">
           <div
             class="absolute -right-60 -top-44 h-60 w-[36rem] transform-gpu md:right-0 
                         bg-[linear-gradient(115deg,var(--tw-gradient-stops))] 
